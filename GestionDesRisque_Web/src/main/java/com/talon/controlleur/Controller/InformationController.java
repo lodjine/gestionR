fa077e8c246412b25915292eef65ee5bc88f320c
med92 <med.nefzi.92@gmail.com>
2016-05-18 13:56:14 +0100

    commit

--- GestionDesRisque_Web/src/main/java/com/talon/controlleur/Controller/InformationController.java
+++ GestionDesRisque_Web/src/main/java/com/talon/controlleur/Controller/InformationController.java
@@ -2,9 +2,21 @@ package com.talon.controlleur.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping(value = "/home/GlManagment")
+
 public class InformationController {
 
+	
+	
+	@RequestMapping(value = "/InformationAdd", method = RequestMethod.GET)
+	public ModelAndView addInformation(){
+		
+		ModelAndView model = new ModelAndView("Process/Information/Information") ; 
+		return model ; 
+		
+		
+	}
 }
